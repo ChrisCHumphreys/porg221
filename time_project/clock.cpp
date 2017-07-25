996040bfea622a432aa6c0a524f7ab69aefecd21
ChrisH <geochris35@gmail.com>
2017-07-25 16:25:59 -0500

    hello world

---
+++ time_project/clock.cpp
@@ -0,0 +1,11 @@
+//Name: Chris Humphreys and Denver Crittenden
+//Date: 07/25/16
+//Description: Clock Project
+
+#include <iostream>
+using namespace std;
+
+int main()
+	cout << "Hello World!" << endl;
+	return 0;
+{
